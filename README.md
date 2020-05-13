3b989b02f7a2844042f9285ea0aaabb882cba13d
Marcelo Roca <mru@marceloroca.com>
2020-05-13 17:31:38 -0400

    add unity test project update to RPR SDK 1.35.1

--- README.md
+++ README.md
@@ -4,10 +4,14 @@ Radeon ProRender Unity Viewport
 (This project is in early stages)
 
 # Prerequisites
-* **AMD Radeon™ ProRender SDK** [link](https://www.amd.com/en/technologies/sdk-agreement)
+* **AMD Radeon™ ProRender SDK** [link](https://github.com/GPUOpen-LibrariesAndSDKs/RadeonProRenderSDK)
+* **OORPR.net [link](https://github.com/marceloroca/rpr.net)
 
 # Instructions
+Install "Editor Coroutines" package in Unity Editor (use Unity Package Manager)
 
+# Unity RPR Viewport
+Open the viewport in Unity Editor Menu: Extensions->RPR4U->Render viewport
 
-
-
+# Important!
+This an early prototype, only diffuse materials are working
--- src/RPR4U.RPRUnityEditor/SceneData.cs
+++ src/RPR4U.RPRUnityEditor/SceneData.cs
@@ -1,4 +1,23 @@
-﻿using RadeonProRender;
+﻿// GPL 3 License
+//
+// Copyright (c) 2020 Marcelo Roca <mru@marceloroca.com>
+//
+// This file is part of RPR4U (Radeon ProRender for Unity).
+//
+// RPR4U is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RPR4U is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with RPR4U. If not, see<https://www.gnu.org/licenses/>.
+
+using RadeonProRender;
 using RadeonProRender.OORPR;
 using System.Collections.Generic;
 using System.IO;
